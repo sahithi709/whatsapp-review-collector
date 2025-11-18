471c376b976677380ad01bfaa0b7b0542275c9b5
sahithi709 <your-email@example.com>
2025-11-18 19:04:25 +0530

    Add frontend source files

---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+import ReviewList from "./components/ReviewList";
+
+function App() {
+  return <ReviewList />;
+}
+
+export default App;
---
+++ frontend/src/components/ReviewList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ReviewList() {
+  return (
+    <div>
+      <h1>WhatsApp Reviews</h1>
+      <p>Review list component</p>
+    </div>
+  );
+}
+
+export default ReviewList;
